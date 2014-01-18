698c354465e834075858010accaa3c370523935e
Tom Horton <thomasphorton@gmail.com>
2014-01-18 17:26:07 -0500

    Added a grunt watch task for minifying css.

--- gruntfile.js
+++ gruntfile.js
@@ -16,19 +16,49 @@ module.exports = function(grunt) {
 
     watch: {
       scripts: {
-        files: ['js/src/**/*.js'],
-        tasks: ['concat', 'uglify'],
+        files: ['public/css/style.css'],
+        tasks: ['cssmin'],
         options: {
           spawn: false,
-        },
+        }
       } 
-    }
+    },
+
+    cssmin: {
+      combine: {
+        files: {
+          'public/css/style.min.css': ['public/css/style.css']
+        }
+      }
+    },
+
+    concat: {
+
+      dist: {
+        src: [
+            'public/css/style.css'
+        ],
+        dest: 'public/css/style.concat.css'
+      },
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%=pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */',
+        separator: ';\n\n'
+      }
+    },
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['imagemin']);
+  grunt.registerTask('css', ['cssmin']);
+
+  grunt.registerTask('default', ['watch']);
 
 };
