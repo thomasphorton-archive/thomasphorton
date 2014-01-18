9dea58902ca1bed658eb3ec984b21989612ee9e5
Tom Horton <thomasphorton@gmail.com>
2014-01-18 14:24:54 -0500

    Removed jquery from pages that don't need it, added initial scale meta tag.

--- app.js
+++ app.js
@@ -19,19 +19,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
   res.status(404);
 
-  // respond with html page
   if (req.accepts('html')) {
     res.render('404', { url: req.url });
     return;
   }
 
-  // respond with json
   if (req.accepts('json')) {
     res.send({ error: 'Not found' });
     return;
   }
 
-  // default to plain-text. send()
   res.type('txt').send('Not found');
 });
 
